50e8fe494e17fe4da958295a0a0b0b4aee87594d
yasmithasai13-afk <yasmithasai13@gmail.com>
2026-06-28 10:49:46 +0530

    Refactor simple calculator into a class structure
    Refactor calculator functions into a Calculator class with error handling for division by zero.

--- simple calci.py
+++ simple calci.py
@@ -1,12 +1,21 @@
-num1,num2=map(int,input("enter two numbers separated by space:").split())
-operation=input("enter operation (+,-,*,/):")
-if operation=="+":
-    print("Result:",num1+num2)
-elif operation=="-":
-    print("Result:",num1-num2)
-elif operation=="*":
-    print("Result:",num1*num2)
-elif operation=="/":
-    print("Result:",num1/num2)
-else:
-    print("invalid operator")    
+class Calculator:
+    def __init__(self):
+        self.num1, self.num2 = map(int, input("enter two numbers separated by space:").split())
+        self.operation = input("enter operation (+,-,*,/):")
+
+    def calculate(self):
+        if self.operation == "+":
+            return self.num1 + self.num2
+        elif self.operation == "-":
+            return self.num1 - self.num2
+        elif self.operation == "*":
+            return self.num1 * self.num2
+        elif self.operation == "/":
+            if self.num2 == 0:
+                return "Undefined"
+            else:
+                return self.num1 / self.num2
+        else:
+            return "Invalid operator"
+obj=Calculator()
+print("Result:", obj.calculate())
